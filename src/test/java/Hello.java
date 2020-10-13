e047b471b7ef003ebe9c0bba82a5b5b0aa9f9b60
iulgur <66178062+iulgur@users.noreply.github.com>
2020-10-13 19:42:34 +0200

    coworker made this change and pushed the github
    this will conflict with my local changes

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        System.out.println("Still really My co-worker added awesome hello"); //we are stimulating the situation with my co-worker
+        System.out.println("Still really My co-worker added awesome hello"); //we are stimulating the situation with my co-worker!!!
 
 
     }
