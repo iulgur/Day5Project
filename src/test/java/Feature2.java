7dff7804b5040d3f2b36ad75fc112006ad8bb96e
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-13 18:47:21 +0200

    awesome feature2 commit more

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -2,6 +2,7 @@ public class Feature2 {
 
     public static void main(String[] args) {
         System.out.println("Working on Feature2");
+        System.out.println("Working on Feature2 more");
     }
 
 
