4a2d52276d2d7231cffe4bfc24d2eef78b95dc6a
Ilyas Ulgur <ulgur@yahoo.com>
2020-10-13 19:32:10 +0200

    added comments on the same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,9 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        System.out.println("Still really awesome hello");
+        System.out.println("Still really awesome hello"); //add one more line on your local and commit.
+
+
     }
 }
 
