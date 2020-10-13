0bdd8992648f85a106b0ca21b385ab98a1e8b086
iulgur <66178062+iulgur@users.noreply.github.com>
2020-10-13 19:26:22 +0200

    Added from GitHub "Hello"

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        System.out.println("Still really awesome hello");
+        System.out.println("Still really FROM GITHUB MADE SOME CHANGES awesome hello");
     }
 }
 
