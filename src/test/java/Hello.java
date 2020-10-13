29fdd1ad6bd232a554e87fa808b60d55d7f754d6
iulgur <66178062+iulgur@users.noreply.github.com>
2020-10-13 19:39:50 +0200

    added more changes by co-worker

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
 
-        System.out.println("Still really awesome hello"); //add one more line on your local and commit.
+        System.out.println("Still really My co-worker added awesome hello"); //we are stimulating the situation with my co-worker
 
 
     }
